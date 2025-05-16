f7e7aaaa6713bcd0828c855f898084443d39ea86
IgorKoval <igorkoval999@gmail.com>
2025-05-16 17:50:45 +0300

    0.78.0

--- sold/src/lib.rs
+++ sold/src/lib.rs
@@ -1,3 +1,15 @@
+/*
+ * Copyright (C) 2022-2025 EverX. All Rights Reserved.
+ *
+ * Licensed under the SOFTWARE EVALUATION License (the "License"); you may not use
+ * this file except in compliance with the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the  GNU General Public License for more details at: https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
 use std::fmt;
 use std::fs::File;
 use std::io::Write;
